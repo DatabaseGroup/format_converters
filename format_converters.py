723d8c2b270bddf26b2a1bbaea125b2ad6bb01ca
Mateusz Pawlik <mpawlik@cosy.sbg.ac.at>
2017-10-18 16:04:32 +0200

    format_converters.py: A working script.

--- format_converters.py
+++ format_converters.py
@@ -1,3 +1,60 @@
-import tree_formats.bracket.dot.converter
-import tree_formats.newick.bracket.converter
-import tree_formats.xml.bracket.converter
+"""
+The MIT License (MIT)
+Copyright (c) 2017 Mateusz Pawlik.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import sys
+import argparse
+import tree_formats.bracket.dot.converter as BDConverter
+import tree_formats.newick.bracket.converter as NBConverter
+import tree_formats.xml.bracket.converter as XBConverter
+
+def main(argv):
+    _BD = 'bracket-dot'
+    _NB = 'newick-bracket'
+    _XB = 'xml-bracket'
+    parser = argparse.ArgumentParser(
+        description='Format converter of the Database Research Group. \
+                     Description can be found in README.md.')
+    parser.add_argument(
+        'source',
+        type=str,
+        help='Source string to convert.'
+    )
+    parser.add_argument(
+        'conversion_type',
+        type=str,
+        choices=[_BD, _NB, _XB],
+        help='Conversion type.'
+    )
+    args = parser.parse_args()
+
+    if args.conversion_type == _BD:
+        BDConverter.convert(args.source)
+
+    if args.conversion_type == _NB:
+        NBConverter.convert(args.source)
+
+    if args.conversion_type == _XB:
+        XBConverter.convert(args.source)
+
+if __name__ == '__main__':
+  main(sys.argv)
